b6ad29fc5ac4653f4f888c0ea804c45921cd5ec2
silverjin <50380898+silverjin@users.noreply.github.com>
2019-05-07 21:52:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+$('.grid-pane').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 320
+  GridPane.setHalignment(node, HPos.CENTER);
+  GridPane.setValignment(node, VPos.CENTER);
+});
