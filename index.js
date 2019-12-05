3ecaabbe632c2dbe1b21e3c3ecba2caf180f4780
silverjin <50380898+silverjin@users.noreply.github.com>
2019-12-05 16:09:27 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-
-$('.grid-pane').masonry({
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 320
-  GridPane.setHalignment(node, HPos.CENTER);
-  GridPane.setValignment(node, VPos.CENTER);
-});
