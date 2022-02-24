7046c1ad4cfe62a1e3be765437bde0894d835271
silverjin <50380898+silverjin@users.noreply.github.com>
2022-02-24 17:38:36 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-// Active isotope with jQuery code:
+const imgContent = document.querySelectorAll('.img-content-hover');
 
+function showImgContent(e) {
+  for(var i = 0; i < imgContent.length; i++) {
+    x = e.pageX;
+    y = e.pageY;
+    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  }
+};
 
-var $grid = $('.grid').imagesLoaded( function() {
-  // init Isotope after all images have loaded
-  $grid.isotope({
-    itemSelector: '.grid-item',
-	  percentPosition: true,
-    gutter: 0,
-    masonry: {
-        // use outer width of grid-sizer for columnWidth
-        columnWidth: '.grid-sizer'
-    }
-  });
-});
-
-// Isotope click function
-$('.iso-nav a').click(function(){
-	$('.iso-nav a').removeClass('active');
-	$(this).addClass('active');
-
-	var selector = $(this).attr('data-filter');
-	$('.grid').isotope({
-		filter: selector
-	});
-	return false;
-});
+document.addEventListener('mousemove', showImgContent);
